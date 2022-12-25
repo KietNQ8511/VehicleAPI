b3541044b16de662820eb161a1e50b3cb658565a
kiet8511@gmail.com <kiet8511@gmail.com>
2022-12-25 23:50:26 +0700

    some

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,35 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = PricingController.class)
+public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	PricingService pricingService;
+	
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void getPrice() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/services/price?vehicleId=1"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+}
